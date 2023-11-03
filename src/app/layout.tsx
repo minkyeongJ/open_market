1ed22e592965f0fd34857bcec5aeec030e86ff12
minkyeongJ <jomk2221@naver.com>
2023-11-03 14:46:28 +0900

    💄 DesignCss: 글로벌 폰트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,28 @@
-import "./globals.css";
+import { Nanum_Gothic } from 'next/font/google'
+import './globals.css'
+// import './style.css'
+
+const nanum_gothic = Nanum_Gothic({
+  weight: ['400', '700', '800'],
+  style: ['normal'],
+  subsets: ['latin'],
+  variable: '--font-nanum-gothic',
+  display: 'swap',
+})
+
+export const metadata = {
+  title: 'open-market',
+  description: '오픈 마켓입니다.',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html>
-      <body>{children}</body>
+    <html lang="ko">
+      <body className={nanum_gothic.className}>{children}</body>
     </html>
-  );
+  )
 }
