0ba30f0a843fe0e1aa5ce031c6d3f8e7bded316a
minkyeongJ <jomk2221@naver.com>
2023-11-13 23:47:55 +0900

    📝 Apply favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Nanum_Gothic } from 'next/font/google'
 import './globals.css'
-// import './style.css'
+import './style.css'
 
 const nanum_gothic = Nanum_Gothic({
   weight: ['400', '700', '800'],
@@ -21,7 +21,30 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="ko">
+    <html lang="ko-KR">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#3da5f5" />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body className={nanum_gothic.className}>{children}</body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <>
       <h1>Home</h1>
       <p>테스트 텍스트</p>
+      <i></i>
     </>
   )
 }
