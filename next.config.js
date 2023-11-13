fa2dfca0c68af89318c3dfc1bf7373000c89661e
minkyeongJ <jomk2221@naver.com>
2023-11-13 22:13:51 +0900

    🔧 sass 설치

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  // sassOptions: {
+  //   includePaths: [path.join(__dirname, 'styles')],
+  // },
+}
 
 module.exports = nextConfig
